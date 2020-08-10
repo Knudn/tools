146ec78c4676cebfcb48ffc263ce2905555a059d
Knudn <kor@kroland.no>
2020-08-09 20:11:03 -0400

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/aircrack-ng/rtl8188eus.git
+cd rtl8188eus
+echo "blacklist r8188eu" > "/etc/modprobe.d/realtek.conf"
+make && make install
+rmmod 8188eu
+modprobe 8188eu
